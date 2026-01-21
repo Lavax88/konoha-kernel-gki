5e1840566d1393f942bdc2b92c05f705473b8e30
Jianpeng Chang <jianpeng.chang.cn@windriver.com>
2026-01-21 13:29:26 +0800

    Bluetooth: MGMT: Fix memory leak in set_ssp_complete
    commit 1b9c17fd0a7fdcbe69ec5d6fe8e50bc5ed7f01f2 upstream.
    
    Fix memory leak in set_ssp_complete() where mgmt_pending_cmd structures
    are not freed after being removed from the pending list.
    
    Commit 302a1f674c00 ("Bluetooth: MGMT: Fix possible UAFs") replaced
    mgmt_pending_foreach() calls with individual command handling but missed
    adding mgmt_pending_free() calls in both error and success paths of
    set_ssp_complete(). Other completion functions like set_le_complete()
    were fixed correctly in the same commit.
    
    This causes a memory leak of the mgmt_pending_cmd structure and its
    associated parameter data for each SSP command that completes.
    
    Add the missing mgmt_pending_free(cmd) calls in both code paths to fix
    the memory leak. Also fix the same issue in set_advertising_complete().
    
    Fixes: 302a1f674c00 ("Bluetooth: MGMT: Fix possible UAFs")
    Signed-off-by: Jianpeng Chang <jianpeng.chang.cn@windriver.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -1943,7 +1943,9 @@ static void set_ssp_complete(struct hci_dev *hdev, void *data, int err)
 			new_settings(hdev, NULL);
 		}
 
-		mgmt_cmd_status(cmd->sk, cmd->index, cmd->opcode, mgmt_err);		return;
+		mgmt_cmd_status(cmd->sk, cmd->index, cmd->opcode, mgmt_err);
+		mgmt_pending_free(cmd);
+		return;
 	}
 
 	if (enable) {
@@ -1960,6 +1962,7 @@ static void set_ssp_complete(struct hci_dev *hdev, void *data, int err)
 		sock_put(match.sk);
 
 	hci_update_eir_sync(hdev);
+	mgmt_pending_free(cmd);
 }
 
 static int set_ssp_sync(struct hci_dev *hdev, void *data)
@@ -6454,6 +6457,7 @@ static void set_advertising_complete(struct hci_dev *hdev, void *data, int err)
 		hci_dev_clear_flag(hdev, HCI_ADVERTISING);
 
 	settings_rsp(cmd, &match);
+	mgmt_pending_free(cmd);
 	new_settings(hdev, match.sk);
 
 	if (match.sk)
